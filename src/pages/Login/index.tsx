66c901882eb7ac98d3e11abcee59ce324dd1fd47
Caio Arruda <47371713+arrudacaio@users.noreply.github.com>
2022-07-03 19:45:54 -0300

    Setup

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Button, Input } from '../../components/General';
+import {
+  CenteredContainer,
+  LinksContainer,
+  Form,
+  Header
+} from '../../styles/general';
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState('email@mail.com')
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <>
-      <h1>Login 1</h1>
-    </>
+    <CenteredContainer>
+      <Header> Acesse sua conta no LovedPet</Header>
+      <Form>
+        {/* <Input type="email" placeholder="Digite seu e-mail"/> */}
+        <Button text="Acessar" typeButton="submit" isLoading={isLoading} />
+      </Form>
+    </CenteredContainer>
+
   )
 };
 
---
+++ src/pages/Login/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Header = styled.head`
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  align-items: left;
+  gap: 32px;
+
+  img {
+    max-height: 150px;
+  }
+
+`;
